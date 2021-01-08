653abf9d38d43ebd1d7b89b2648ffb8af7977dcc
Rafael <rafaelyokoyama925@gmail.com>
2021-01-08 12:29:10 -0300

    feito pag index

--- Games e Animes/js/main.js
+++ Games e Animes/js/main.js
@@ -1,6 +1,28 @@
+
 $(document).ready(function () {
     $(".hamburguer").click(function () {
       $(this).toggleClass("hamburguer-active");
       $(".itens-mobile").toggleClass("active");
     });
   })
+
+
+
+
+  
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
